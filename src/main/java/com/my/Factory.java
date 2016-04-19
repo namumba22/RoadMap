fd790a3484a8345a01ccafc13e4a4f550a413da0
namumba22 <namumba22@gmail.com>
2016-04-20 00:05:02 +0300

    sadfasdf asdf asd f as df asdf  as df with '#' will be ignored, and an empty message aborts the commit.

--- src/main/java/com/my/Factory.java
+++
@@ -1,9 +0,0 @@
-package com.my;
-
-import java.io.IOException;
-import java.util.*;
-
-public class Factory {
-
-
-}
--- src/main/java/com/my/RoadDao.java
+++
@@ -1,9 +0,0 @@
-package com.my;
-
-public class RoadDao {
-    private String name;
-
-    private City from;
-    private City to;
-
-}
